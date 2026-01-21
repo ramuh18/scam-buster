6afeaa6a40ff344fe548bdb02f59eca58a835c86
ramuh18 <160219860+ramuh18@users.noreply.github.com>
2026-01-21 15:22:47 +0900

    Update app_kr.py

--- app_kr.py
+++ app_kr.py
@@ -190,4 +190,4 @@ with col_b:
     )
 
 st.markdown("<br>", unsafe_allow_html=True)
-st.caption("© 2026 ScamBuster AI. (쿠팡 파트너스 활동 수수료 포함)")
+st.caption("© 2026 ScamBuster AI. (이 포스팅은 쿠팡 파트너스 활동의 일환으로, 이에 따른 일정액의 수수료를 제공받습니다)")
